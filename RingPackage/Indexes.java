7c4127f7c305a18fb0475d548ac4eac92e299f53
Vidyut Veedgav <vidyut.veedgav@gmail.com>
2023-09-26 11:35:58 -0400

    Refresh Commit

--- RingPackage/Indexes.java
+++ RingPackage/Indexes.java
@@ -9,16 +9,15 @@ import java.util.stream.Stream;
  * @author Vidyut Veedgav
  * a record representing the index of a matrix
  */
-public record Indexes<T>(int row, int column) implements Comparable<Indexes<T>> {
+public record Indexes(int row, int column) implements Comparable<Indexes> {
 
     /**
      * overriding the compareTo method of Comparable
      */
     @Override
-    public int compareTo(Indexes<T> o) {
+    public int compareTo(Indexes o) {
         //null check
         Objects.requireNonNull(o, "o cannot be null");
-
         if (this.row > o.row) {
             return 1;
         }
@@ -30,13 +29,19 @@ public record Indexes<T>(int row, int column) implements Comparable<Indexes<T>> 
         }
     }
 
+    @Override
+    public String toString() {
+        return (row() + ", " + column());
+    }
+
     /**
      * a helper method to compare indexes by column
      * @param o
      * @return
      */
-    private int compareComlumns(Indexes<T> o) {
+    private int compareComlumns(Indexes o) {
 
+        //null check
         assert o != null : "o cannot be null";
         if (column > o.column) {
             return 1;
@@ -54,7 +59,7 @@ public record Indexes<T>(int row, int column) implements Comparable<Indexes<T>> 
      * @param matrix
      * @return
      */
-    public T value(T[][] matrix) {
+    public <T> T value(T[][] matrix) {
         return matrix[row][column];
     }
 
@@ -63,18 +68,13 @@ public record Indexes<T>(int row, int column) implements Comparable<Indexes<T>> 
      * @return true if diagonal
      */
     public boolean areDiagonal() {
-        if (row == column) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return row() == column();
     }
 
     /**
      * a static method to generate all the indexes in a given range
      */
-    public static <T> Stream<Indexes<T>> stream(Indexes<T> from, Indexes<T> to) {
+    public static <T> Stream<Indexes> stream(Indexes from, Indexes to) {
         //null checks
         Objects.requireNonNull(from, "from cannot be null");
         Objects.requireNonNull(from, "to cannot be null");
@@ -89,31 +89,31 @@ public record Indexes<T>(int row, int column) implements Comparable<Indexes<T>> 
         return IntStream.rangeClosed(minRow, maxRow)
                         .boxed() //wraps the contents of the range into Integers
                         .flatMap((row) -> IntStream.rangeClosed(minColumn, maxColumn) //use flat map to flatten the 2D stream into a single stream
-                        .mapToObj(column -> new Indexes<>(row, column))); //mapping each row column pair to a new Index
+                        .mapToObj(column -> new Indexes(row, column))); //mapping each row column pair to a new Index
     }
 
     /**
      * overloading the stream mehtod to generate indexes between (0, 0) and the size endpoint
      */
-    public static <T> Stream<Indexes<T>> stream(Indexes<T> size) {
+    public static Stream<Indexes> stream(Indexes size) {
         //null checks
         Objects.requireNonNull(size, "size cannot be null");
         
-        return stream(new Indexes<T>(0, 0), size);    
+        return stream(new Indexes(0, 0), size);    
     }
 
     /**
      * overloading the stream mehtod to generate indexes between (0, 0) and the (rows, columns) endpoint
      */
-    public static <T> Stream<Indexes<T>> stream(int rows, int columns) {
+    public static <T> Stream<Indexes> stream(int rows, int columns) {
 
-        return stream(new Indexes<T>(0, 0), new Indexes<T>(rows, columns)); 
+        return stream(new Indexes(0, 0), new Indexes(rows, columns)); 
     }
 
     public static void main(String[] args) {
 
-        Indexes<Integer> index = new Indexes<>(1, 2);
-        Indexes<Integer> index2 = new Indexes<>(3, 4);
+        Indexes index = new Indexes(1, 2);
+        Indexes index2 = new Indexes(3, 4);
         System.out.println(Indexes.stream(index, index2).collect(Collectors.toList()));
     }
 }
--- RingPackage/MatrixMap.java
+++ RingPackage/MatrixMap.java
@@ -1,10 +1,12 @@
 package RingPackage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Vidyut Veedgav
@@ -12,13 +14,13 @@ import java.util.function.Function;
  */
 public final class MatrixMap<T> {
     
-    private final Map<Indexes<T>, T>  matrix; //an instance field representing the matrix
+    private final Map<Indexes, T>  matrix; //an instance field representing the matrix
     
     /**
      * a constructor for the matrix
      * @param matrix
      */
-    private MatrixMap(Map<Indexes<T>, T> matrix) {
+    private MatrixMap(Map<Indexes, T> matrix) {
         //null check
         assert matrix != null : "matrix cannot be null";
         this.matrix = matrix;
@@ -28,12 +30,26 @@ public final class MatrixMap<T> {
      * a method to return the matrix size
      * @return
      */
-    public Indexes<T> size() {
+    public Indexes size() {
 
-        List<Indexes<T>> keys = new ArrayList<>(matrix.keySet());
-        Indexes<T> maxIndex = keys.get(0);
+        List<Indexes> keys = new ArrayList<>(matrix.keySet());
+        Indexes maxIndex = keys.get(0);
 
-        for (Indexes<T> key : keys)  {
+        maxIndex = findMax(keys, maxIndex);
+        return maxIndex;
+    }
+
+    /**
+     * a helper method which iterates through the matrix and returns the maximum key value
+     * @param keys
+     * @param maxIndex
+     * @return
+     */
+    private Indexes findMax(List<Indexes> keys, Indexes maxIndex) {
+
+        assert keys != null : "keys cannot be null";
+        assert maxIndex != null : "maxIndex cannot be null";
+        for (Indexes key : keys)  {
             if (maxIndex.compareTo(key) > 0) {
                 maxIndex = key;
             }
@@ -41,21 +57,33 @@ public final class MatrixMap<T> {
         return maxIndex;
     }
 
-    
-
     /**
      * a method to override the toString method
      */
     @Override
     public String toString() {
-        return super.toString();
+
+        Indexes size = size();
+        int numRows = size.row() + 1; // Add 1 to include row 0
+        int numCols = size.column() + 1; // Add 1 to include column 0
+    
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Indexes index = new Indexes(row, col);
+                T value = matrix.get(index);
+                System.out.println("[" + index.toString() + "]: " + value); //print the entry
+                System.out.println("\t"); //print a tab
+            }
+            System.out.println("\n");
+        }
+        return "";
     }
 
     /**
      * a method to return the element at the given indexes
      * @return the corresponding value
      */
-    public T value(Indexes<T> indexes) {
+    public T value(Indexes indexes) {
 
         Objects.requireNonNull(indexes, "indexes cannot be null");
         return matrix.get(indexes);
@@ -67,52 +95,144 @@ public final class MatrixMap<T> {
      */
     public T value(int row, int column) {
 
-        Indexes<T> index  = new Indexes<>(row, column);
+        Indexes index  = new Indexes(row, column);
         return matrix.get(index);
     }
 
-    /* Indexes<T> Indexes::value(MatrixMap<T> matrix) {
+    //WHAT IS THIS
+    public Indexes value(MatrixMap<T> matrix) {
         //wtf is this method
         return null;
     }
-    */
     
+    /**
+     * a nested class to aid error handling by defining protocols for controlling the matrix lengths
+     */
     static class InvalidLengthException extends Exception {
 
-          enum Cause {
+
+        /**
+         * an enum to define the possible values of the exception's cause
+         */
+        enum Cause {
             ROW, 
             COLUMN
         }
+    
+        private Cause cause; //a private variable declaring the cause
+        private Integer length; //a private variable declaring the length
 
-        private Cause cause;
-        private Integer length;
-
+        /**
+         * a constructor for the InvalidLengthException
+         * @param cause
+         * @param length
+         */
         public InvalidLengthException(Cause cause, Integer length) {
             this.cause = cause;
             this.length = length;
         }
 
-        /*
-        public Cause getCause() {
+        /**
+         * getter for the cause
+         * @return the cause
+         */
+        public Cause cause() {
             return cause;
         }
-        */
 
-        public Integer getLength() {
+        /**
+         * a getter for the length
+         * @return the length
+         */
+        public Integer length() {
             return length;
         }
 
+        /**
+         * a method to check if the length is positive and throw an exception otherwise
+         * @param cause
+         * @param length
+         * @return the length
+         */
         public static int requireNonEmpty(Cause cause, int length) {
 
             if (length > 0) {
-                throw new IllegalArgumentException(); //must make cause of this IllegalLengthException
+                throw new IllegalArgumentException("InvalidLengthException"); //must make cause of this InvalidLengthException
             }
             return length;
         }
     }
 
-    public static <S> MatrixMap<S> instance (int rows, int columns, Function<Indexes<?>, S> valueMapper) {
+    /**
+     * a method to create an instance of a MatrixMap using a defined valueMapper function
+     * @param <S>
+     * @param rows
+     * @param columns
+     * @param valueMapper
+     * @return a new MatrixMap
+     */
+    public static <S> MatrixMap<S> instance (int rows, int columns, Function<Indexes, S> valueMapper) {
+
+        //error handling
+        checkPositive(rows, columns);
+        
+        Map<Indexes, S> matrix = new HashMap<>();
+        List<Indexes> indexes = Indexes.stream(rows, columns).collect(Collectors.toList());
+
+        for (Indexes index : indexes) {
+            S value = valueMapper.apply(index);
+            matrix.put(index, value);
+        }
+        return new MatrixMap<>(matrix);
+    }
+
+    /**
+     * a helper method to determine if arguments are negative and throw the appropriate exception
+     * @param rows
+     * @param columns
+     */
+    private static void checkPositive(int rows, int columns) {
+        if (rows <= 0 || columns <= 0) {
+            throw new IllegalArgumentException(); //MUST HAVE THE RIGHT CAUSE
+        }
+    }
+
+    /**
+     * overloading the instance method to accept an Indexes object as a parameter
+     * @param <S>
+     * @param size
+     * @param valueMapper
+     * @return a MatrixMap
+     */
+    public static <S> MatrixMap<S> instance(Indexes size, Function<Indexes, S> valueMapper) {
+
+        //null checks
+        Objects.requireNonNull(size, "size cannot be null");
+        Objects.requireNonNull(valueMapper, "valueMapper cannot be null");
+
+        //extracting the row and column values from the Indexes parameter
+        int row = size.row();
+        int column = size.column();
+
+        //calling the original instance method
+        return MatrixMap.instance(row, column, valueMapper);
+    }
+
+    public static <S> MatrixMap<S> constant(int size, S value) {
+
+        //null check
+        Objects.requireNonNull(value, "value cannot be null");
+        if (size <= 0) {
+            throw new IllegalArgumentException();
+        }
+        Map<Indexes, S> matrix = new HashMap<>();
         return null;
+    }
+
+    public static void main(String[] args) {
+
+        //example: storing values as the sum of the index row and column
+        System.out.println(MatrixMap.instance(1, 2, (index) -> (index.row() + index.column())).toString());
     }
 }
 
